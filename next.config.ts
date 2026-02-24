62db5c73a9459e7796b4595b1ecd8a2697b245da
Amreen Fathima A <fathimaamreen351@gmail.com>
2026-02-24 16:13:24 +0530

    updated domain in config file

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["firebasestorage.googleapis.com","images.pexels.com"],
+    domains: ["firebasestorage.googleapis.com", "images.pexels.com", "img.freepik.com"],
   },
 };
 
